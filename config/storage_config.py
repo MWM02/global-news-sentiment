2682c601af0eaa37914c2d01ff123fc672b9f8e4
MWM02 <wijem004@gmail.com>
2025-12-03 21:49:54 +0000

    refactor: add local variables

--- config/storage_config.py
+++ config/storage_config.py
@@ -1,17 +1,24 @@
 from typing import TypedDict
 
+PATH_FOR_RAW_DATA = "data/raw"
+PATH_FOR_CLEAN_DATA = "data/clean"
+SOURCES_FILE_NAME = "source.csv"
+ARTICLES_FILE_NAME = "articles.csv"
+
 
 class StorageConfig(TypedDict):
-    output_dir: str
-    sources_csv: str
-    combined_csv: str
+    output_dir_for_raw_data: str
+    output_dir_for_clean_data: str
+    sources_file_name: str
+    articles_file_name: str
 
 
 def load_storage_config() -> StorageConfig:
     config: StorageConfig = {
-        "output_dir": "data/raw",
-        "sources_csv": "sources.csv",
-        "articles_csv": "articles_combined.csv",
+        "output_dir_for_raw_data": PATH_FOR_RAW_DATA,
+        "output_dir_for_clean_data": PATH_FOR_CLEAN_DATA,
+        "sources_file_name": SOURCES_FILE_NAME,
+        "articles_file_name": ARTICLES_FILE_NAME,
     }
 
     return config
