68157950742a65258803b7c1bb101f8b5ac0f3d1
MWM02 <wijem004@gmail.com>
2025-12-03 21:54:03 +0000

    fix: correct import for test env

--- src/extract/extract.py
+++ src/extract/extract.py
@@ -2,6 +2,7 @@ import os
 import pandas as pd
 from config.api_config import load_api_config
 from config.storage_config import load_storage_config
+from config.etl_config import load_etl_config
 from src.extract.extract_sources import extract_sources
 from src.extract.extract_articles import extract_articles
 from src.utils.file_utils import save_and_append_to_csv
@@ -10,35 +11,55 @@ from src.utils.logging_utils import setup_logger
 logger = setup_logger("extract_data", "extract_data.log")
 
 
-def extract_data() -> tuple[pd.DataFrame, pd.DataFrame]:
-    env = os.getenv("ENV", "unknown")
+def extract_data(env: str) -> tuple[pd.DataFrame, pd.DataFrame]:
     try:
-        api_config = load_api_config()
         storage_config = load_storage_config()
 
         if env == "test":
-            sources_df = pd.read_csv("data/raw/sources.csv")
-            articles_df = pd.read_csv("data/raw/articles_combined.csv")
+            raw_sources_file_path = os.path.join(
+                storage_config["output_dir_for_raw_data"],
+                storage_config["sources_file_name"],
+            )
+            raw_articles_file_path = os.path.join(
+                storage_config["output_dir_for_raw_data"],
+                storage_config["articles_file_name"],
+            )
+
+            try:
+                sources_df = pd.read_csv(raw_sources_file_path)
+                articles_df = pd.read_csv(raw_articles_file_path)
+
+            except FileNotFoundError as e:
+                logger.exception(f"File not found: {e.filename}")
+                raise
+
+            logger.info("Data extraction skipped... Using local data.")
 
         elif env == "dev":
+            api_config = load_api_config()
+            etl_config = load_etl_config()
             sources_df = extract_sources(api_config)
             save_and_append_to_csv(
                 sources_df,
-                storage_config["output_dir"],
-                storage_config["sources_csv"],
-            )
-            sources_df = pd.read_csv("./data/raw/sources.csv")
-            articles_df = extract_articles(sources_df, api_config)
-            save_and_append_to_csv(
-                articles_df,
-                storage_config["output_dir"],
-                storage_config["articles_csv"],
+                storage_config["output_dir_for_raw_data"],
+                storage_config["sources_file_name"],
             )
 
-        logger.info(
-            f"Data extraction completed successfully - "
-            f"Sources: {sources_df.shape}, Articles: {articles_df.shape}"
-        )
+            articles_df = extract_articles(sources_df, api_config, etl_config)
+            save_and_append_to_csv(
+                articles_df,
+                storage_config["output_dir_for_raw_data"],
+                storage_config["articles_file_name"],
+            )
+
+            logger.info(
+                f"Data extraction completed successfully - "
+                f"Sources: {sources_df.shape}, Articles: {articles_df.shape}"
+            )
+
+        else:
+            logger.error(f"Unknown environment: {env}")
+            raise
 
         return sources_df, articles_df
 
