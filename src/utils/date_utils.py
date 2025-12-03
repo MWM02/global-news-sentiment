0439d76daeb529718af56c47e886ce0c7c22d517
MWM02 <wijem004@gmail.com>
2025-12-03 22:04:53 +0000

    fix: avoid use of config class

--- src/utils/date_utils.py
+++ src/utils/date_utils.py
@@ -1,7 +1,5 @@
 from datetime import datetime, timedelta
-from config.api_config import ApiConfig
 
 
-def get_article_date_str(api_config: ApiConfig) -> str:
-    days_before = api_config.get("days_before", 2)
-    return (datetime.now() - timedelta(days=days_before)).strftime("%d-%m-%Y")
+def get_date_str(days_back: int) -> str:
+    return (datetime.now() - timedelta(days=days_back)).strftime("%Y-%m-%d")
